c57825abbc73e5b472a4811cdbddfbaf220017ce
Vasyl Korol mac <gusak.guska223@gmail.com>
2022-04-11 18:12:59 +0200

    first commit

---
+++ src/main/java/com/example/recipesrest/RecipesRestApplication.java
@@ -0,0 +1,13 @@
+package com.example.recipesrest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RecipesRestApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RecipesRestApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/recipesrest/RecipesRestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.recipesrest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RecipesRestApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
